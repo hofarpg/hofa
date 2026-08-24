a6e793c935620cc57da79a6d1a0c8921d59594a1
maple <103380848+starguiders@users.noreply.github.com>
2026-08-24 17:34:32 -0600

    Add user alias processing and sorting functionality
    This script processes user aliases, creating two arrays: one with spaces and one without. It also sorts the user list alphabetically and appends it to the HTML.

---
+++ hofauserlist.js
@@ -0,0 +1,39 @@
+/* automatic alias additions by LUX */
+var members = []; //this is the array of names
+var membersNS = []; //this is the array of names without spaces
+$(".mbmember").each(function () {
+  var member = $(this).find(".mbm-user div.field_uneditable").html().toLowerCase().trim();
+  var memberArray = member.split(" ");
+  var memberNS = ""; //set an empty variable for now
+  if (member != "") {
+    //if the member alias isn't empty, run the loop
+    for (var i = 0; i < memberArray.length; i++) {
+      //for each word in the member's alias, add the word to the empty variable so that it's a version without spaces
+      memberNS = memberNS + memberArray[i];
+    }
+    if (jQuery.inArray(member, members) == -1) {
+      //if it's not a duplicate, add the version WITH spaces to the members array for the label text
+      members.push(member);
+    }
+    if (jQuery.inArray(memberNS, membersNS) == -1) {
+      //same as above, but the no space version to the array to be used for data filter calls
+      membersNS.push(memberNS);
+    }
+  }
+  $(this).addClass(memberNS);
+});
+//for each member in the array, add them to the html inside the filtergroup with a class of mfilt-user
+for (var i = 0; i < members.length; i++) {
+    if (members[i] != undefined) {
+
+         $('.mfilt-user').append('<li class="abc" data-filter=".u-' + membersNS[i] + '">' + members[i] + '</li>');
+    }
+}
+
+/* ordenar usuarios por orden alfabético */
+$(".mfilt-user li.abc").sort(asc_sort).appendTo(".mfilt-user");
+//$("#debug").text("Output:");
+// accending sort
+function asc_sort(a, b) {
+  return $(b).text() < $(a).text() ? 1 : -1;
+}
