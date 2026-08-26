3f38e274d53053f8ca1d203bca3350c371e24640
NamiStuff <36270268+NamiStuff@users.noreply.github.com>
2026-08-26 16:46:11 +0200

    Refactor member alias addition

--- hofauserlist.js
+++ hofauserlist.js
@@ -1,41 +1,41 @@
 setTimeout(function(){
-/* automatic alias additions by LUX */
-var members = []; //this is the array of names
-var membersNS = []; //this is the array of names without spaces
-$(".mbmember").each(function () {
-  var member = $(this).find(".mbm-user div.field_uneditable").html().toLowerCase().trim();
-  var memberArray = member.split(" ");
-  var memberNS = ""; //set an empty variable for now
-  if (member != "") {
-    //if the member alias isn't empty, run the loop
-    for (var i = 0; i < memberArray.length; i++) {
-      //for each word in the member's alias, add the word to the empty variable so that it's a version without spaces
-      memberNS = memberNS + memberArray[i];
+  /* automatic alias additions by LUX */
+  var members = []; //this is the array of names
+  var membersNS = []; //this is the array of names without spaces
+  $(".mbmember").each(function () {
+    var member = $(this).find(".mbm-user div.field_uneditable").text().trim().toLowerCase().replace(/\s+/g, "-").normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    var memberArray = member.split(" ");
+    var memberNS = ""; //set an empty variable for now
+    if (member != "") {
+      //if the member alias isn't empty, run the loop
+      for (var i = 0; i < memberArray.length; i++) {
+        //for each word in the member's alias, add the word to the empty variable so that it's a version without spaces
+        memberNS = memberNS + memberArray[i];
+      }
+      if (jQuery.inArray(member, members) == -1) {
+        //if it's not a duplicate, add the version WITH spaces to the members array for the label text
+        members.push(member);
+      }
+      if (jQuery.inArray(memberNS, membersNS) == -1) {
+        //same as above, but the no space version to the array to be used for data filter calls
+        membersNS.push(memberNS);
+      }
     }
-    if (jQuery.inArray(member, members) == -1) {
-      //if it's not a duplicate, add the version WITH spaces to the members array for the label text
-      members.push(member);
-    }
-    if (jQuery.inArray(memberNS, membersNS) == -1) {
-      //same as above, but the no space version to the array to be used for data filter calls
-      membersNS.push(memberNS);
-    }
+    $(this).addClass(memberNS);
+  });
+  //for each member in the array, add them to the html inside the filtergroup with a class of mfilt-user
+  for (var i = 0; i < members.length; i++) {
+      if (members[i] != undefined) {
+  
+           $('.mfilt-user').append('<li><a data-filter-value=".u-' + membersNS[i] + '">' + members[i] + '</a></li>');
+      }
   }
-  $(this).addClass(memberNS);
-});
-//for each member in the array, add them to the html inside the filtergroup with a class of mfilt-user
-for (var i = 0; i < members.length; i++) {
-    if (members[i] != undefined) {
-
-         $('.mfilt-user').append('<li class="abc"><a data-filter-value=".u-' + membersNS[i] + '">' + members[i] + '</a></li>');
-    }
-}
-
-/* ordenar usuarios por orden alfabético */
-$(".mfilt-user li.abc").sort(asc_sort).appendTo(".mfilt-user");
-//$("#debug").text("Output:");
-// accending sort
-function asc_sort(a, b) {
-  return $(b).text() < $(a).text() ? 1 : -1;
-}  
-}, 600);
+  
+  /* ordenar usuarios por orden alfabético */
+  $(".mfilt-user li.abc").sort(asc_sort).appendTo(".mfilt-user");
+  //$("#debug").text("Output:");
+  // accending sort
+  function asc_sort(a, b) {
+    return $(b).text() < $(a).text() ? 1 : -1;
+  }  
+}, 300);
